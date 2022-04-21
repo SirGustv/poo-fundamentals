140ecd15cc60bacb89e8c0bb374c84329aebf8ff
Gustavo <gust.bersan@gmail.com>
2022-04-20 22:36:43 -0300

    Instanciando Classe Herói
    A classe Herói irá ser um molde para declarar os personagens.

--- RPGComPOO/Program.cs
+++ RPGComPOO/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using RPGComPOO.scr.Entities;
 
 namespace RPGComPOO
 {
@@ -6,7 +7,7 @@ namespace RPGComPOO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
---
+++ RPGComPOO/src/Entities/Hero.cs
@@ -0,0 +1,12 @@
+
+
+namespace RPGComPOO.scr.Entities
+{
+
+    public class Hero
+    {
+        public string Name;
+        public int Level;
+        public string TypeHero;
+    }
+}
