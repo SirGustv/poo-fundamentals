ef727f9947639e90981dad8c3a92f048a3f2f2c8
Gustavo <gust.bersan@gmail.com>
2022-04-24 13:13:17 -0300

    InstânciandoHP e MP

--- RPGComPOO/src/Entities/Person.cs
+++ RPGComPOO/src/Entities/Person.cs
@@ -5,21 +5,26 @@ namespace RPGComPOO.src.Entities
 
     public class Person
     {
-        public Person(string name, int level, string typePerson)
+        public Person(string name, int level, string typePerson, string hP, string mP)
         {
             this.Name = name;
             this.Level = level;
             this.TypePerson = typePerson;
+            this.MP = mP;
+            this.HP = hP;
         }
+
         public Person() { }
 
         public string Name;
         public int Level;
         public string TypePerson;
+        public string HP;
+        public string MP;
 
         public override string ToString()
         {
-            return this.Name + " " + this.Level + " " + this.TypePerson;
+            return this.Name + "\n Lv." + this.Level + " " + this.TypePerson + "\n HP " + this.HP + "\n MP " + this.MP;
         }
         public virtual string Attack()
         {
