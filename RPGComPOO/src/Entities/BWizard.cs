9ca80a40ff2a7eb27b9f547bb1e44ce84f1023fb
Gustavo <gust.bersan@gmail.com>
2022-04-24 20:16:50 -0300

    Instânciando classe Black Wizard
    Classe já instânciada totalmente atualizada com os ataques para este personagem

---
+++ RPGComPOO/src/Entities/BWizard.cs
@@ -0,0 +1,34 @@
+namespace RPGComPOO.src.Entities
+{
+    public class BWizard : Person
+    {
+        public BWizard(string name, int level, string typePerson, string hP, string mP)
+        {
+            this.Name = name;
+            this.Level = level;
+            this.TypePerson = typePerson;
+            this.HP = hP;
+            this.MP = mP;
+        }
+        public BWizard()
+        {
+
+        }
+        public override string Attack()
+        {
+            return this.Name + " lançou magia negra!";
+        }
+        public override string Attack(int bonus)
+        {
+            if (bonus > 6)
+            {
+                return "'Buraco negro' com bônus de " + bonus;
+            }
+            else
+            {
+                return "'Náusea' com bônus de " + bonus;
+            }
+
+        }
+    }
+}
