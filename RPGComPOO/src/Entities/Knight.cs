3fec78c9091f138129387e28dd86af9f333c2cee
Gustavo <gust.bersan@gmail.com>
2022-04-24 13:15:56 -0300

    Atualizando a classe Knight
    Adicionando os pontos de vida e mana ao personagem Knight

--- RPGComPOO/src/Entities/Knight.cs
+++ RPGComPOO/src/Entities/Knight.cs
@@ -2,10 +2,18 @@ namespace RPGComPOO.src.Entities
 {
     public class Knight : Person
     {
-        public Knight(string name, int level, string typePerson) : base(name, level, typePerson)
+        public Knight(string name, int level, string typePerson, string hP, string mP)
         {
+            this.Name = name;
+            this.Level = level;
+            this.TypePerson = typePerson;
+            this.HP = hP;
+            this.MP = mP;
         }
+        public Knight()
+        {
 
+        }
         public override string Attack()
         {
             return this.Name + " Atacou com sua espada.";
