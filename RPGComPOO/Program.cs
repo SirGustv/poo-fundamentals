636fa5c65320ae913ab2090a54b26dd7b42295c4
Gustavo <gust.bersan@gmail.com>
2022-04-24 17:04:53 -0300

    Chamando o Layout

--- RPGComPOO/Program.cs
+++ RPGComPOO/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using RPGComPOO.src.Entities;
+using RPGComPOO.src.Layout;
 
 namespace RPGComPOO
 {
@@ -7,12 +8,7 @@ namespace RPGComPOO
     {
         static void Main(string[] args)
         {
-            Knight arus = new Knight("Arus", 29, "Knight", "489/500", "20/20");
-            Wizard jenica = new Wizard("Jenica", 30, "White Wizard", "180/200", "460/500");
-
-            Console.WriteLine(jenica);
-            Console.WriteLine(arus);
-
+            Interface.Show();
         }
     }
 }
