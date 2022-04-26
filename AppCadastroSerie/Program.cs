6ac9b5fe02ea913717679e902a69eac1c120741b
Gustavo <gust.bersan@gmail.com>
2022-04-25 22:11:59 -0300

    Criando arquivos para novo projeto
    Projeto que visa criar uma interface de cadastro de séries e filmes.

---
+++ AppCadastroSerie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppCadastroSerie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cadastro de séries...");
+        }
+    }
+}
