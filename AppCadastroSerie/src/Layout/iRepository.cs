38887adc412659f81bd2785d126bf1c17e6bf345
Gustavo <gust.bersan@gmail.com>
2022-05-06 20:53:13 -0300

    Criando interface de repositório para o projeto

---
+++ AppCadastroSerie/src/Layout/iRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace AppCadastroSerie.Layout
+{
+    public interface iRepository<T>
+    {
+        List<T> Lista();
+        T ReturnById(int id);
+        void Insert(T entity);
+        void Exclude(int id);
+        void Update(int id, T entity);
+
+        int NextId();
+    }
+}
