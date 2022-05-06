f9fa0e7307c5928c2c2e8a4af7d1459f8e769fd3
Gustavo <gust.bersan@gmail.com>
2022-05-06 19:48:37 -0300

    Criando método construtor para informações das séries

--- AppCadastroSerie/src/Entities/Series.cs
+++ AppCadastroSerie/src/Entities/Series.cs
@@ -3,8 +3,17 @@ namespace AppCadastroSerie
     public class Series : BaseEntity
     {
         private Genre Genre { get; set; }
-        private string Tttle { get; set; }
+        private string Title { get; set; }
         private string Description { get; set; }
         private int Year { get; set; }
+
+        public Series(int id, Genre genre, string title, string description, int year)
+        {
+            this.Id = id;
+            this.Genre = genre;
+            this.Title = title;
+            this.Description = description;
+            this.Year = year;
+        }
     }
 }
