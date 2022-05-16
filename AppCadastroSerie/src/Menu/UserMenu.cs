0a4d8529426fe6fc6e8df7f388ab1e5b3c7df36c
Gustavo <gust.bersan@gmail.com>
2022-05-15 23:21:47 -0300

    Criando método Listar Séries

--- AppCadastroSerie/src/Menu/UserMenu.cs
+++ AppCadastroSerie/src/Menu/UserMenu.cs
@@ -1,15 +1,43 @@
 using System;
+using AppCadastroSerie.Classes;
 
 namespace AppCadastroSerie.Menu
 {
     class UserMenu
     {
+        static SerieRepository repositorio = new SerieRepository();
         public static void CallMenu()
         {
-            UserOptions();
+            string userOption = GetUserOptions();
+            while (userOption.ToUpper() != "X")
+            {
+                switch (userOption)
+                {
+                    case "1":
+                        ListSeries();
+                        break;
+                }
+            }
         }
 
-        private static void UserOptions()
+        private static void ListSeries()
+        {
+            Console.WriteLine("Lista de Séries");
+
+            var lista = repositorio.Lista();
+
+            if (lista.Count == 0)
+            {
+                Console.WriteLine("Nenhuma série encontrada.");
+            }
+            foreach (var serie in lista)
+            {
+                Console.WriteLine("#ID {0}: - {1} ", serie.returnId(), serie.returnTitle());
+            }
+
+        }
+
+        private static string GetUserOptions()
         {
             Console.Clear();
 
@@ -22,6 +50,10 @@ namespace AppCadastroSerie.Menu
             Console.WriteLine("4 - Visualizar série");
             Console.WriteLine("5 - Excluir série");
             Console.WriteLine("X - Sair");
+
+            string userOption = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return userOption;
         }
     }
 }
